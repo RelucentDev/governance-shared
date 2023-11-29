1434256f760c6123a733673e5e083bde501e9285
Dom Webber <dom.webber@hotmail.com>
2023-11-29 21:16:35 +0000

    refactor: introduce file signature comments

--- packages/governance/index.js
+++ packages/governance/index.js
@@ -1,1 +1,11 @@
+/**
+ * Relucent Governance.
+ *
+ * @since     1.0.0
+ * @module    Relucent\Governance
+ * @copyright 2023 Relucent Ltd
+ * @author    Relucent Ltd <hello@relucent.dev>
+ * @see       https://relucent.dev
+ */
+
 module.exports = "(c) Relucent Ltd";

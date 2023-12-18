ecc5235a8c436f480fc0eefd85a5324e884bba06
Dom Webber <dom.webber@hotmail.com>
2023-12-18 08:12:16 +0000

    refactor: migrate to turborepo monorepo

---
+++ packages/eslint-config/src/eslint-plugin-jsdoc.d.ts
@@ -0,0 +1,16 @@
+/**
+ * Relucent ESLint Config.
+ *
+ * @since     1.0.0
+ * @copyright 2023 Relucent Ltd
+ * @author    Relucent Ltd <hello@relucent.dev>
+ * @see       https://relucent.dev
+ */
+
+/**
+ * ESLint JSDoc Plugin.
+ *
+ * @since 1.0.0
+ * @see {@link https://github.com/gajus/eslint-plugin-jsdoc}
+ */
+declare module "eslint-plugin-jsdoc";

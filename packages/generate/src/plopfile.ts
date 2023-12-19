d11fdf55ba2c3472d06838ca315ec9198255b75d
Dom Webber <dom.webber@hotmail.com>
2023-12-19 19:37:24 +0000

    feat: add templating for README files

--- packages/generate/src/plopfile.ts
+++ packages/generate/src/plopfile.ts
@@ -7,14 +7,21 @@
  * @see       https://relucent.dev
  */
 import acknowledgements from "./acknowledgements/index.js";
+import readme from "./readme/index.js";
 import { NodePlopAPI } from "plop";
 import pluralize from "pluralize";
+import title from "title";
 
 export default function (plop: NodePlopAPI) {
   // Plop Config
   plop.setWelcomeMessage("@relucent.dev/generators");
   plop.setHelper("pluralize", (text: string) => pluralize(text));
+  plop.setHelper("titleCase", (text: string) => title(text));
+  plop.setHelper("inArray", (array: string[], value: string) =>
+    array.includes(value),
+  );
 
   // Generators
   acknowledgements(plop);
+  readme(plop);
 }
---
+++ packages/generate/src/readme/index.ts
@@ -0,0 +1,63 @@
+/**
+ * Relucent Generators.
+ *
+ * @since     1.0.0
+ * @copyright 2023 Relucent Ltd
+ * @author    Relucent Ltd <hello@relucent.dev>
+ * @see       https://relucent.dev
+ */
+import { NodePlopAPI } from "plop";
+
+export default function readme(plop: NodePlopAPI) {
+  plop.setGenerator("readme", {
+    description: "Generate an README.md file",
+    prompts: [
+      {
+        type: "input",
+        name: "projectName",
+      },
+      {
+        type: "input",
+        name: "projectIntroduction",
+      },
+      {
+        type: "input",
+        name: "projectAuthor",
+        default: "Relucent Ltd",
+      },
+      {
+        type: "input",
+        name: "projectEmail",
+        default: "hello@relucent.dev",
+      },
+      {
+        type: "list",
+        name: "projectLicense",
+        default: "Proprietary",
+        choices: [
+          "Proprietary",
+          "GNU LGPLv3",
+          "MIT",
+          "GNU GPLv3",
+          "Apache-2.0",
+          "BSD-3-Clause",
+        ],
+      },
+      {
+        type: "checkbox",
+        name: "includeSections",
+        choices: ["Installation", "Usage"],
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "README.md",
+        templateFile: "../templates/README.md.hbs",
+        data: {
+          year: new Date().getFullYear(),
+        },
+      },
+    ],
+  });
+}

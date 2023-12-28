97bee01ca58f9da2ced6c11ebf829883e86d7f1c
Dom Webber <dom.webber@hotmail.com>
2023-12-28 20:38:11 +0000

    refactor: prefer cjs file over JSON

---
+++ packages/prettier-config/index.cjs
@@ -0,0 +1,28 @@
+/**
+ * Relucent Prettier Config.
+ *
+ * @since     1.0.0
+ * @copyright 2023 Relucent Ltd
+ * @author    Relucent Ltd <hello@relucent.dev>
+ * @see       https://relucent.dev
+ */
+
+/** @type {import("prettier").Config & import("@trivago/prettier-plugin-sort-imports").PluginConfig} */
+module.exports = {
+  importOrder: [
+    "",
+    "^react(-dom)?$",
+    "^next(/.*|$)",
+    "<THIRD_PARTY_MODULES>",
+    "^[./]",
+    "^(@|\\d|_)",
+    "<THIRD_PARTY_TS_TYPES>",
+    "<TS_TYPES>^[./]",
+  ],
+  plugins: [
+    "@trivago/prettier-plugin-sort-imports",
+    "@prettier/plugin-xml",
+    "prettier-plugin-sh",
+    "prettier-plugin-pkg",
+  ],
+};
